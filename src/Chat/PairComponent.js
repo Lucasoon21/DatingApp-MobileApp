6e92d1e5b7245d47249508feacf477db74121100
Lucasoon <wolski1111@gmail.com>
2021-10-13 22:44:43 +0200

    create chat screen etc

---
+++ src/Chat/PairComponent.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, Linking, Platform, ScrollView, TouchableOpacity } from 'react-native';
+import Menu from "../control/Menu";
+import DetailsProfileScreen from '../DetailsProfile/DetailsProfileScreen';
+import { styles, contact } from '../Chat/style';
+import MenuTop from '../Unused/MenuTop';
+import { FontAwesome } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+const PairComponent = (props) => {
+    const goProfile = () => props.navigation.navigate("ProfileUser")
+    const goChat = () => props.navigation.navigate("Chat")
+
+    return (
+        <>
+            <View style={contact.container}>
+                <TouchableOpacity onPress={goProfile}>
+
+                    <View style={contact.left}>
+                        <Image source={require('../Images/person1.jpg')} style={contact.image} />
+                        <View style={contact.textContainer} >
+                            <Text style={contact.text} numberOfLines={1}>Łukasz, 22</Text>
+                            <Text style={contact.text} numberOfLines={1}>22 km stąd</Text>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={goChat}>
+                    <Entypo name="chevron-thin-right" size={40} color="black" />
+                </TouchableOpacity>
+
+            </View>
+
+
+
+
+
+
+
+
+
+            {/* <View style={[contact.container, { justifyContent: "flex-start" }]}>
+            <Image source={require('../Images/person1.jpg')} style={contact.image} />
+            <View style={{ display: 'flex', alignItems: 'center', marginLeft: 30 }}>
+
+                <Text style={contact.text}>Łukasz, 22</Text>
+                <Text style={contact.text}>22 km stąd</Text>
+            </View>
+        </View>
+
+        <View style={contact.container}>
+            <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+
+                <Image source={require('../Images/person4.jpeg')} style={[contact.image, { marginRight: 10 }]} />
+
+                <Text style={contact.text}>Krystyrddfgfdg, 22</Text>
+            </View>
+            <Text style={contact.text}>22 km stąd</Text>
+        </View> */}
+
+
+        </>
+    );
+};
+export default PairComponent;
